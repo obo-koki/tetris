d018f189471ea6f7894cd35a2df924ca4fe31348
obo-koki <koki-40589@outlook.jp>
2022-09-15 06:51:42 +0900

    Adjust param

--- game_manager/genetic_algorithm/ga_config.py
+++ game_manager/genetic_algorithm/ga_config.py
@@ -1,10 +1,10 @@
 #GA parameter
 NIND = 9
-NGEN = 30
-POP = 100
+NGEN = 20
+POP = 50
 CXPB = 0.6
 MUTPB = 0.2
 #The tetris game parameter to learning
 GAME_LEVEL = 3
-GAME_TIME = 5
+GAME_TIME = 30
 DROP_INTERVAL = 1
